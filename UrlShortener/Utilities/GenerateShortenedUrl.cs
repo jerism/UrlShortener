200126a52cfb65d73f744076561ff6db31f2d209
Michael O'Donnell <Michael.ODonnell@deltatre.com>
2022-05-24 22:18:04 +0100

    added check to ensure uid is unique

--- UrlShortener/Utilities/GenerateShortenedUrl.cs
+++ UrlShortener/Utilities/GenerateShortenedUrl.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Database.Interfaces;
 using Microsoft.Extensions.Configuration;
 
 namespace UrlShortener.Utilities
@@ -8,9 +9,11 @@ namespace UrlShortener.Utilities
         private readonly string _characters;
         private readonly int _charactersLength;
         private readonly Random _random = new();
+        private readonly IUrlRepository _urlRepository;
 
-        public GenerateShortenedUrl(IConfiguration config)
+        public GenerateShortenedUrl(IConfiguration config, IUrlRepository urlRepository)
         {
+            _urlRepository = urlRepository ?? throw new ArgumentNullException(nameof(urlRepository));
             _characters = config.GetValue<string>("BaseCharacters") ?? throw new NullReferenceException(nameof(_characters));
             _charactersLength = _characters.Length;
         }
@@ -25,13 +28,16 @@ namespace UrlShortener.Utilities
         {
             var uniqueId = string.Empty;
 
-            // TODO validate identifier is unique
-
             for (var i = 0; i < 6; i++)
             {
                 var index = _random.Next(_charactersLength);
                 var character = _characters[index];
                 uniqueId += character;
+            }
+
+            if (_urlRepository.UniqueIdentiferExists(uniqueId))
+            {
+                uniqueId = CreateUniqueIdentifier();
             }
 
             return uniqueId;
